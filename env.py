96b39458f5a42ed76970d01b1027d7b25d291ee6
VictorKuzinov <96018937+VictorKuzinov@users.noreply.github.com>
2023-07-21 13:48:11 +0500

    Add files via upload

---
+++ env.py
@@ -0,0 +1,52 @@
+#env.py
+from sqlalchemy import engine_from_config, MetaData
+from alembic import context
+
+# Это будет вызываться Alembic сценарием для каждого шага миграции.
+# Здесь мы инициализируем подключение к базе данных на основе параметров из конфигурации env.py.
+
+# Создаем подключение к базе данных
+url = f"postgresql://postgres:Diamond_12@localhost/Task one"
+engine = engine_from_config(context.config.get_section('alembic'), url=url)
+
+# Привязываем подключение к контексту
+connection = engine.connect()
+
+# Создаем объект MetaData и связываем его с контекстом
+metadata = MetaData()
+metadata.bind = engine
+
+# Связывание объекта MetaData с контекстом
+context.configure(
+    connection=connection,
+    target_metadata=metadata
+)
+
+# Применяем миграции к базе данных, используя созданное подключение
+def run_migrations_offline():
+    context.configure(
+        url=url,
+        target_metadata=None,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+# Применяем миграции к базе данных, используя созданное подключение
+def run_migrations_online():
+    with engine.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=metadata  # Use the same 'metadata' object here
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+# В зависимости от окружения, вызываем соответствующую функцию применения миграций
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
