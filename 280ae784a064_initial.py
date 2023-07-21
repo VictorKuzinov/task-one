fef1f008d32dbe4ad2f179eba4a8174dde1f2c4e
VictorKuzinov <96018937+VictorKuzinov@users.noreply.github.com>
2023-07-21 14:21:43 +0500

    alembic/versions/280ae784a064_initial
    Миграция.

---
+++ 280ae784a064_initial.py
@@ -0,0 +1,24 @@
+# 280ae784a064_initial.py
+"""Initial migration
+
+Revision ID: 280ae784a064
+Revises:
+Create Date: 2023-07-19  # Дата создания файла
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# Здесь должно быть объявление переменной revision
+revision = '280ae784a064'
+down_revision = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
