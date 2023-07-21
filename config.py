17115f09aa52d3dd4a2115f150e179bd8311506b
VictorKuzinov <96018937+VictorKuzinov@users.noreply.github.com>
2023-07-21 20:04:36 +0500

    Файл конфигурации для подключения к базе данных

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+DB_HOST = 'localhost'
+DB_PORT = 5432
+DB_USER = 'postgres'
+DB_PASSWORD = YourPassword
+DB_DATABASE = 'YourDatabase'
+DATABASE_URL = 'postgresql://postgres:YourPassword@localhost/YourDatabse'
+HOST=8080
+SECRET_KEY='YourSecretKey'
